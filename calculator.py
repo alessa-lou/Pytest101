e2926dc294f78542c39a4b16a91d53002f9adf87
alessa-lou <alessa_hardwick@live.co.uk>
2020-11-03 15:26:10 +0000

    gives selection of operations, user can input integer values and calculates result

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
+import sys
+import numbers
+
 class Calculator:
     """ a calc """
 
@@ -12,3 +15,23 @@ class Calculator:
 
     def divide(self, a, b):
         return a / b
+
+if __name__ == "__main__":
+    print("we shall calculate")
+    calculator = Calculator()
+    operations = [
+        calculator.add,
+        calculator.subtract,
+        calculator.multiply,
+        calculator.divide,
+    ]
+    while True:
+        for i, operation in enumerate(operations, start=1):
+            print(f"{i}: {operation.__name__}")
+            operation = input("Pick an operation: ")
+            if operation == "q":
+                sys.exit()
+            op = int(operation)
+            a = float(input("pick a number: "))
+            b = float(input("pick another number: "))
+            print(operations[op - 1](a, b))
